faa91f5bf7deec118594f9055e45298abde71a2c
Anuraag <baishya.anuraag1920@gmail.com>
2016-12-27 14:39:36 +0530

    Add to Firebase

---
+++ app/src/main/java/com/appex/bartobusiness/Fragment/QRFragment.java
@@ -0,0 +1,76 @@
+package com.appex.bartobusiness.Fragment;
+
+import android.graphics.Bitmap;
+import android.os.Bundle;
+import android.support.v4.app.DialogFragment;
+import android.support.v4.content.ContextCompat;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+
+import com.appex.bartobusiness.R;
+import com.google.zxing.BarcodeFormat;
+import com.google.zxing.MultiFormatWriter;
+import com.google.zxing.WriterException;
+import com.google.zxing.common.BitMatrix;
+
+public class QRFragment extends DialogFragment {
+
+    public final static int QRcodeWidth = 500;
+    Bitmap bitmap;
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+
+        View rootView = inflater.inflate(R.layout.fragment_qr, container, false);
+        ImageView imageView = (ImageView) rootView.findViewById(R.id.qr_image_view);
+        String key = getArguments().getString("Key");
+        try{
+
+            bitmap = textToImageEncode(key);
+            imageView.setImageBitmap(bitmap);
+        }
+        catch (WriterException e){
+            e.printStackTrace();
+        }
+        return rootView;
+    }
+
+    Bitmap textToImageEncode(String Value) throws WriterException {
+        BitMatrix bitMatrix;
+        try {
+            bitMatrix = new MultiFormatWriter().encode(
+                    Value,
+                    BarcodeFormat.DATA_MATRIX.QR_CODE,
+                    QRcodeWidth, QRcodeWidth, null
+            );
+
+        } catch (IllegalArgumentException Illegalargumentexception) {
+
+            return null;
+        }
+        int bitMatrixWidth = bitMatrix.getWidth();
+
+        int bitMatrixHeight = bitMatrix.getHeight();
+
+        int[] pixels = new int[bitMatrixWidth * bitMatrixHeight];
+
+        for (int y = 0; y < bitMatrixHeight; y++) {
+            int offset = y * bitMatrixWidth;
+
+            for (int x = 0; x < bitMatrixWidth; x++) {
+
+                pixels[offset + x] = bitMatrix.get(x, y) ?
+                        ContextCompat.getColor(getActivity(), R.color.black) :
+                        ContextCompat.getColor(getActivity(), R.color.white);
+            }
+        }
+        Bitmap bitmap = Bitmap.createBitmap(bitMatrixWidth, bitMatrixHeight, Bitmap.Config.ARGB_4444);
+
+        bitmap.setPixels(pixels, 0, 500, 0, 0, bitMatrixWidth, bitMatrixHeight);
+        return bitmap;
+    }
+
+}
