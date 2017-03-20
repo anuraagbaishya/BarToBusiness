986e4be0d85ade37ccfad2031ab6291a3041a555
Anuraag <baishya.anuraag1920@gmail.com>
2017-03-20 20:38:25 +0530

    Intro Done

--- app/src/main/java/com/appex/barcards/activities/AppIntroActivity.java
+++ app/src/main/java/com/appex/barcards/activities/AppIntroActivity.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 import android.support.v4.content.res.ResourcesCompat;
 
 import com.appex.barcards.R;
+import com.appex.barcards.utils.Strings;
 import com.github.paolorotolo.appintro.AppIntro;
 import com.github.paolorotolo.appintro.AppIntroFragment;
 
@@ -14,12 +15,22 @@ public class AppIntroActivity extends AppIntro {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        addSlide(AppIntroFragment.newInstance("XYZ", "ABC", R.drawable.bcbanner, ResourcesCompat.getColor(getResources(),R.color.colorPrimary, null)));
+        addSlide(AppIntroFragment.newInstance(Strings.addTitle, Strings.addDesc, R.drawable.add, ResourcesCompat.getColor(getResources(), R.color.colorPrimary, null)));
+        addSlide(AppIntroFragment.newInstance(Strings.scanTitle, Strings.scanDesc, R.drawable.scan, ResourcesCompat.getColor(getResources(), R.color.colorPrimary, null)));
+        addSlide(AppIntroFragment.newInstance(Strings.manageTitle, Strings.manageDesc, R.drawable.connect, ResourcesCompat.getColor(getResources(), R.color.colorPrimary, null)));
     }
 
     @SuppressWarnings("deprecation")
     @Override
-    public void onDonePressed(){
+    public void onDonePressed() {
+
+        startActivity(new Intent(getApplicationContext(), LoginActivity.class));
+        finish();
+    }
+
+    @SuppressWarnings("deprecation")
+    @Override
+    public void onSkipPressed() {
 
         startActivity(new Intent(getApplicationContext(), LoginActivity.class));
         finish();
